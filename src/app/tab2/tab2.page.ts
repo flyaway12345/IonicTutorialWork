147bdbb6f8eb1e5b9c00dd059e5f05effc0af3ae
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2024-03-27 16:13:14 -0400

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonIcon, IonFabButton, IonCol, IonRow, IonGrid, IonImg } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonImg, IonGrid, IonRow, IonCol, IonFabButton, IonIcon, IonFab, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,CommonModule]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -10,7 +10,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+defineCustomElements(window);
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
